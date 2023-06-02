1617bf36063a5a1a6631f90230df3a659fd018a6
Murilo-Bob <Murilohue779@gmail.com>
2023-06-02 10:23:55 -0300

    Adicionado o código da bala

--- SpaceShooter/Assets/Scripts/BulletControl.cs
+++ SpaceShooter/Assets/Scripts/BulletControl.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class BulletControl : MonoBehaviour
 {
+    [SerializeField] private float vel = 7f;
     private Rigidbody2D myRb;
-    [SerializeField] private float vel = 5f;
     // Start is called before the first frame update
     void Start()
     {
         myRb = GetComponent<Rigidbody2D>();
+        myRb.velocity = new Vector2(0, 1) * vel;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
-        Vector2 myVel = new Vector2(horizontal, vertical).normalized * vel;
-
-        myRb.velocity = myVel;
+        
     }
 }
