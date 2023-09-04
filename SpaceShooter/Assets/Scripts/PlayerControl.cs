2c00a5366f47005ff364bdfdc0bed4536f0d9926
Murilo-Bob <Murilohue779@gmail.com>
2023-09-04 15:44:21 -0300

    new bullets  + levels
    wow so hard omgg

--- SpaceShooter/Assets/Scripts/PlayerControl.cs
+++ SpaceShooter/Assets/Scripts/PlayerControl.cs
@@ -8,11 +8,13 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody2D myRb;
     [SerializeField] private float vel = 5f;
     [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject bullet2;
     [SerializeField] private Transform playerBulletPosition;
     [SerializeField] private GameObject explosion;
     [SerializeField] private float bulletSpeed = 7f;
     [SerializeField] private float xLimit;
     [SerializeField] private float yLimit;
+    [SerializeField] private int bulletLevel = 1;
     private float vertical;
     private float horizontal;
     [SerializeField] private int health = 5;
@@ -50,11 +52,41 @@ public class PlayerControl : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject myBullet = Instantiate(bullet, playerBulletPosition.position, transform.rotation);
-            //Give the direction and speed to the RB of the bullet 
-            myBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0, bulletSpeed);
+            switch (bulletLevel)
+            {
+                case 1:
+                    //The shots are going to be created in the circle positioned in the scene window 
+                    BulletCreate(bullet, playerBulletPosition.position);
+                    break;
+                case 2:
+                    //Using the transform position of the player to manipulate the distance between the shots
+                    float posX = transform.position.x;
+                    float posY = transform.position.y;
+                    //Left shot
+                    BulletCreate(bullet2, new Vector3(posX - 0.4f, posY + 0.1f, 0));
+                    //Right shot
+                    BulletCreate(bullet2, new Vector3(posX + 0.4f, posY + 0.1f, 0));
+                    break;
+                case 3:
+                    //Using both created shots
+                    //middle shot
+                    BulletCreate(bullet, playerBulletPosition.position);
+                    posX = transform.position.x;
+                    posY = transform.position.y;
+                    //Left shot
+                    BulletCreate(bullet2, new Vector3(posX - 0.4f, posY + 0.1f, 0));
+                    //Right shot
+                    BulletCreate(bullet2, new Vector3(posX + 0.4f, posY + 0.1f, 0));
+                    break;
+            }
         }
+    }
 
+    private void BulletCreate(GameObject bullet, Vector3 transformPos)
+    {
+        GameObject myBullet = Instantiate(bullet, transformPos, transform.rotation);
+        //Give the direction and speed to the RB of the bullet 
+        myBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0, bulletSpeed);
     }
 
     public void TakeDamage(int damage)
